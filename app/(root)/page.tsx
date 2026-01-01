40b282b69897f002d59c06d9517616228b1cde6d
Greg Mirabito <30662442+mirabg@users.noreply.github.com>
2026-01-01 18:26:22 -0500

    Force dynamic rendering for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,8 @@ export const metadata = {
   title: "Home",
 };
 
+export const dynamic = 'force-dynamic';
+
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
   return (
