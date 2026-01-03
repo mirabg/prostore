0ad977fe12c4ceccaa670df6b88017af012d08c3
Greg Mirabito <30662442+mirabg@users.noreply.github.com>
2026-01-03 11:08:52 -0500

    Added payment method

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  paymentMethodSchema,
 } from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
@@ -10,7 +11,7 @@ import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatErrors } from "../utils";
 import { ShippingAddress } from "@/types";
-import { success } from "zod";
+import { z } from "zod";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -113,6 +114,39 @@ export async function updateUserAddress(data: ShippingAddress) {
     return {
       success: true,
       messaage: "User shipping address updated successfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatErrors(error),
+    };
+  }
+}
+
+//Update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user.id },
+    });
+
+    if (!currentUser) {
+      throw new Error("User not found");
+    }
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
+    return {
+      success: true,
+      message: "User payment method updated successfully",
     };
   } catch (error) {
     return {
